01b640f84134bd4f3f6ca546a20455a1aec0d884
tobias <tobias.s@lviscorp.com>
2026-06-29 10:50:31 +0900

    fix: gc should be a .sh file

--- GIT-SHORTCUTS.sh
+++ GIT-SHORTCUTS.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 source "$SCRIPT_DIR/git-shortcuts.conf"
 source "$SCRIPT_DIR/get-checkout-ticket-number.sh"
-source "$SCRIPT_DIR/git-commit-ticket-number"
+source "$SCRIPT_DIR/git-commit-ticket-number.sh"
 source "$SCRIPT_DIR/git-pull-current.sh"
 source "$SCRIPT_DIR/git-push-current.sh"
 source "$SCRIPT_DIR/git-remove-closed-branches.sh"
